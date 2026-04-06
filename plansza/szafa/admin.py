bcf963ec86272ba56b310e9077733976a1c34743
Wojtek <kojtek41@gmail.com>
2026-04-06 10:19:49 +0200

    Zadania z listy pierwszej

---
+++ plansza/szafa/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Project, Task, Tag
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "is_active")
+    search_fields = ("name",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "status", "due_date", "created_at")
+    list_filter = ("status", "project", "tags")
+    search_fields = ("title", "description")
